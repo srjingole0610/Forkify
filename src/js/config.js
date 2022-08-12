11a8cf4d4c27f16dce2dd57fd21c9bafd2ed8e9e
srjingole0610 <suraj.ingole0610@gmail.com>
2022-08-12 17:05:25 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '03fc9734-0c5e-4f8a-8252-c29875bc4eb0';
+export const MODAL_CLOSE_SEC = 2.5;
